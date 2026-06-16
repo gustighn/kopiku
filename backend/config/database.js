fabcfbcd0c0c9a0d2225684d5e8acf11000df1b6
gustighn <gustigani903@gmail.com>
2026-06-17 04:51:46 +0800

    .

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,6 +10,12 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: process.env.NODE_ENV === 'production' ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    } : {},
     pool: {
       max: 5,
       min: 0,
